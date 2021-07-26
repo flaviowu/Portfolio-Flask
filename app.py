debda546de5c2860983b01d4c6e70f7a86293a0b
Flavio Wu <flaviowu@gmail.com>
2021-07-26 17:23:37 -0300

    minor changes

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ mail_settings = {
      'MAIL_PORT': 465,
      'MAIL_USE_TLS': False,
      'MAIL_USE_SSL': True,
-     'MAIL_USERNAME': '',
-     'MAIL_PASSWORD':''
+     'MAIL_USERNAME': 'contatoflaviowu@gmail.com',
+     'MAIL_PASSWORD':'xtzmkzjmxrnrmkcp'
  }
 
 app.config.update(mail_settings)
